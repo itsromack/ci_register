2db756901d8ebd35218dc8f1fc4807a185319ba2
Romack Natividad <macki@freelancer.com>
2013-12-09 22:19:58 +0800

    UserHandler as the default route

---
+++ application/controllers/userhandler.php
@@ -0,0 +1,24 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class UserHandler extends CI_Controller {
+    public function index() {
+        $this->load->view('index');
+    }
+
+    public function register() {
+        $this->load->view('register');
+    }
+
+    public function forgot() {
+        $this->load->view('forgot');
+    }
+
+    public function login() {
+        $this->load->view('login');
+    }
+
+    public function dashboard() {
+        $this->load->view('dashboard');
+    }
+}
+
