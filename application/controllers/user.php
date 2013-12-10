a57ca87ca1953c8a51365b76296830f575879b2e
Romack Natividad <macki@freelancer.com>
2013-12-10 19:17:35 +0800

    templating tweak

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -1,24 +1,30 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class User extends CI_Controller {
+    public function show($page) {
+        $this->load->view('includes/header');
+        $this->load->view($page);
+        $this->load->view('includes/footer');
+    }
+
     public function index() {
-        $this->load->view('index');
+        $this->show('index');
     }
 
     public function register() {
-        $this->load->view('register');
+        $this->show('register');
     }
 
     public function forgot() {
-        $this->load->view('forgot');
+        $this->show('forgot');
     }
 
     public function login() {
-        $this->load->view('login');
+        $this->show('login');
     }
 
     public function dashboard() {
-        $this->load->view('dashboard');
+        $this->show('dashboard');
     }
 }
 
